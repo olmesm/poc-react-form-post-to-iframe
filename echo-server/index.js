74002d432185c4ddfd3bf943e663468f8d09d6ea
Oliver Smit <olmesm@gmail.com>
2019-08-08 11:25:27 +0100

    pos complete

--- echo-server/index.js
+++ echo-server/index.js
@@ -1,1 +1,7 @@
-module.exports = async req => req
+const parse = require('urlencoded-body-parser')
+
+module.exports = async req => {
+  const data = await parse(req)
+  console.log(data)
+  return `<h1>${JSON.stringify(data)}<h1>`
+}
--- src/App.js
+++ src/App.js
@@ -1,15 +1,85 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+
+const TIME_OUT_MS = 3000
+const DATA = {
+  name: 'fred'
+}
+
+const visibleStyle = {
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  height: '100vh',
+  width: '100vw',
+  border: 'none',
+  position: 'absolute',
+  background: 'white'
+}
+
+const hiddenStyle = {
+  top: '0',
+  left: '0',
+  height: '0',
+  width: '0',
+  border: 'none',
+  position: 'absolute',
+  overflow: 'hidden'
+}
+
+const PostForm = ({ setSubmitRef, data }) => {
+  const dataElements = Object.keys(data).map((key, index) => (
+    <input defaultValue={data[key]} key={`${key}-${index}`} name={key} />
+  ))
+
+  return (
+    <form
+      style={hiddenStyle}
+      action="http://localhost:3030"
+      method="post"
+      target="output_frame">
+      {dataElements}
+      <button ref={setSubmitRef} />
+    </form>
+  )
+}
+
+const IframePanel = ({ show }) => {
+  return (
+    <iframe
+      style={show ? visibleStyle : hiddenStyle}
+      title="abc"
+      name="output_frame"
+      src=""
+      id="output_frame"></iframe>
+  )
+}
 
 function App() {
+  const [shouldShow, setShouldShow] = useState(false)
+  const [submitRef, setSubmitRef] = useState(undefined)
+
+  const onClick = useCallback(() => {
+    setShouldShow(true)
+    submitRef.click()
+  }, [submitRef])
+
+  useEffect(() => {
+    if (submitRef) {
+      setTimeout(onClick, TIME_OUT_MS)
+    }
+  }, [onClick, submitRef])
+
   return (
-    <div>
-      <button
-        onClick={() => {
-          console.log('yes')
-        }}>
-        Open iframe
-      </button>
-    </div>
+    <>
+      <IframePanel show={shouldShow} />
+
+      <PostForm data={DATA} setSubmitRef={setSubmitRef} />
+
+      <p>Will automagically submit after {TIME_OUT_MS / 1000} seconds</p>
+
+      <button onClick={onClick}>Manually Submit</button>
+    </>
   )
 }
 
